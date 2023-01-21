f1068f5591044544851c7bfc9dfea21af9b9fac6
serickalv <serick.alvju.7@gmail.com>
2023-01-21 18:17:32 -0500

    Avance, falta terminar los dos ultimos ejercicios

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,33 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var letra=nombre.charAt(0);
+  var may=letra.toUpperCase(letra);
+  var arr=[];
+  for (let i=0;i<nombre.length;i++){
+    arr[i]=nombre.charAt(i);
+  }
+  arr.shift();
+  arr.unshift(may);
+  let nuevoNombre='';
+  for (let i=0;i<nombre.length;i++){
+    nuevoNombre+=arr[i];
+  }
+  return nuevoNombre;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
+
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +38,22 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma=0;
+  for(let i=0;i<numeros.length;i++){
+    suma+=numeros[i];
+    }
+  cb(suma);
+
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  for(let i=0; i<array.length;i++)
+  {
+    cb(array[i]);
+  }
 }
 
 function map(array, cb) {
